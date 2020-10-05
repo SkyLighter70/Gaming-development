f84a79113652c0adae918c5cacdf113a0e78eaf1
Skyler <skyler@ainscough.com>
2020-10-05 12:42:13 -0600

    Added proto2 FeedTheAnimals and scripted player movement

---
+++ FeedtheAnimals/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        //block the player at -10
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            
+        }
+        //block the player at 10
+        if(transform.position.x < 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            
+        }
+    }
+}
