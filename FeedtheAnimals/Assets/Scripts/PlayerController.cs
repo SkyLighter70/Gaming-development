755bbad286ee870bbb0c3914c8c112d0e507104f
Skyler <skyler@ainscough.com>
2020-10-07 12:42:53 -0600

    Added boundaries and cookie launching

--- FeedtheAnimals/Assets/Scripts/PlayerController.cs
+++ FeedtheAnimals/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10;
+    public float speed = 10.0f;
+    public float xRange = 25.0f;
+    public GameObject foodProjectile;
     
     // Start is called before the first frame update
     void Start()
@@ -16,19 +18,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    //get input and stores it in horizontalInput
         horizontalInput = Input.GetAxis("Horizontal");
+        //takes the horizontalInput and multiplies by speed and Time.deltaTime to control the speed and have it trigger once per second.
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
-        //block the player at -10
-        if(transform.position.x < -10)
+        //block the player at -xRange
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
             
         }
-        //block the player at 10
-        if(transform.position.x < 10)
+        //block the player at xRange
+        if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
             
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodProjectile, transform.position, foodProjectile.transform.rotation);
         }
     }
 }
