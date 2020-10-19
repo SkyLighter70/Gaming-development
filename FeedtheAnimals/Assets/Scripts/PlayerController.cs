fefed9664f11fee3b9fc047628be54ae7e3fb60f
Skyler <skyler@ainscough.com>
2020-10-19 12:40:11 -0600

    Added collisions and game over.

--- FeedtheAnimals/Assets/Scripts/PlayerController.cs
+++ FeedtheAnimals/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float speed = 25.0f;
     public float xRange = 25.0f;
     public GameObject foodProjectile;
     
--- FeedtheAnimals/Assets/Scripts/SpawnManager.cs
+++ FeedtheAnimals/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 30.0f;
+    
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 //    public int animalIndex;
     
+    private void Start()
+        {
+            InvokeRepeating("SpawnRandAnimal", startDelay, spawnInterval);
+        }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        
+    }
+    void SpawnRandAnimal() 
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             
-        }
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            
     }
 }
