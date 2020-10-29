2a6cc86a8d5ad7d18aa41a261dc5f648e2cd1622
Skyler <skyler@ainscough.com>
2020-10-29 12:39:48 -0600

    Added Fire Delay and Multiple Ball types to Fetch

--- New Unity Project/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ New Unity Project/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,6 +12,7 @@ public class SpawnManagerX : MonoBehaviour
     
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
+    private int ballType = 1;
 //        private int randBallInt = Random.Range(0,2);
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,13 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
-
+        int ballType = Random.Range(0, 3);
+        float spawnInterval = Random.Range(3.0f, 5.0f);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballType], spawnPos, ballPrefabs[ballType].transform.rotation);
     }
 
 }
