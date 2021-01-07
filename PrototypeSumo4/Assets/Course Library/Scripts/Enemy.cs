40917d96ca1429168e5b9d8feaca3d49ce5d5c64
Skyler <skyler@ainscough.com>
2021-01-07 14:02:51 -0700

    Added Enemy Waves, Powerups, and other stuff

--- PrototypeSumo4/Assets/Course Library/Scripts/Enemy.cs
+++ PrototypeSumo4/Assets/Course Library/Scripts/Enemy.cs
@@ -7,7 +7,9 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
+
     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +18,19 @@ public class Enemy : MonoBehaviour
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        
+
     }
+    
 }
