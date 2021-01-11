22347e053c461bd414b2d8699408b19d6e2ec81e
Skyler <skyler@ainscough.com>
2021-01-11 14:48:53 -0700

    Spawning is broken again

--- Lab3PlayerMovement/Assets/Scripts/RockBehav.cs
+++ Lab3PlayerMovement/Assets/Scripts/RockBehav.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class RockBehav : MonoBehaviour
 {
+    public Rigidbody rockRb;
+    public GameObject Sun;
 
+
+    private float speed = 2.0f;
     //variables
-    public float spawnDelaySpeed = 1.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        Sun = GameObject.Find("Sun");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < .5)
+        {
+            Destroy(gameObject);
+        }
+        Vector3 lookDirection = (Sun.transform.position - transform.position).normalized;
+        rockRb.AddForce(lookDirection * speed);
     }
 }
