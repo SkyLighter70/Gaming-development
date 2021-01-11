1d503f42dbf6bfd456bb505157ccf648803ff000
Skyler <skyler@ainscough.com>
2021-01-11 14:06:13 -0700

    Finished lab 3 and started personal project

---
+++ Lab3PlayerMovement/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    //variables
+    public Rigidbody playerRb;
+    public string curLane = "middle";
+    public float mousePosVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GameObject.Find("Player").GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 temp = Input.mousePosition;
+        temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+        playerRb.transform.position = Camera.main.ScreenToWorldPoint(temp);
+
+
+        
+
+    }
+}
