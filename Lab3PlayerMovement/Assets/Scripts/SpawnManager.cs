4e0b5e2a8900827d332ba7a62f3491baba0cdc15
Skyler <skyler@ainscough.com>
2021-01-12 16:13:33 -0700

    Slight change to player art

--- Lab3PlayerMovement/Assets/Scripts/SpawnManager.cs
+++ Lab3PlayerMovement/Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
 
 
     public float startDelay = 2.0f;
-    public float spawnInterval = 1.5f;
+    public float spawnInterval = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
